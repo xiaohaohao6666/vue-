a6660c086ad228456e9d1259acbcaa2f183930ca
xiaohaohao6666 <1286227723@qq.com>
2019-03-10 13:00:45 +0800

    v-1.5 login页面表单

--- src/components/login.vue
+++ src/components/login.vue
@@ -2,18 +2,19 @@
   <div class="login">
     <el-form
       :model="formData"
+      :rules="rules"
       status-icon
       ref="ruleForm2"
       label-width="100px"
       class="demo-ruleForm"
       label-position="top"
     >
-    <h2>用户登录</h2>
+      <h2>用户登录</h2>
       <el-form-item label="用户名" prop="username">
-        <el-input v-model.number="formData.username"></el-input>
+        <el-input v-model.trim="formData.username"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input type="password" v-model="formData.password" autocomplete="off"></el-input>
+        <el-input type="password" v-model.trim="formData.password" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button class="button" type="primary" @click="submitForm('formData')">提交</el-button>
@@ -23,43 +24,55 @@
 </template>
 <script>
 export default {
-    data() {
-      return {
-        formData: {
-          username: '',
-          password: '',
-        },
-      };
-    },
+  data() {
+    return {
+      formData: {
+        username: "",
+        password: ""
+      },
+      // 表单的验证规则
+      rules: {
+        username: [
+          { required: true, message: "请输用户名", trigger: "blur" },
+          { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "change" }
+        ],
+        password: [
+          { required: true, message: "请输密码", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "change" }
+        ],
+      }
+    };
+  }
 };
 </script>
 <style>
-body,html{
-    height: 100%;
-    width: 100%;
+body,
+html {
+  height: 100%;
+  width: 100%;
 }
-body{
-    background-color: #324152;
-    padding: 0;
-    margin: 0;
+body {
+  background-color: #324152;
+  padding: 0;
+  margin: 0;
 }
-body>div {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+body > div {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .login {
-    width: 580px;
-    height: 436px;
-    background-color: #fff;
-    padding: 40px;
-    box-sizing: border-box;
-    border-radius: 10px;
+  width: 580px;
+  height: 436px;
+  background-color: #fff;
+  padding: 40px;
+  box-sizing: border-box;
+  border-radius: 10px;
 }
-.login .button{
-    width: 100%;
+.login .button {
+  width: 100%;
 }
 </style>
 
