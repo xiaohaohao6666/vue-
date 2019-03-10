a88142e72bdd8bda3b4bc5a92f6a3b26febbc6bb
xiaohaohao6666 <1286227723@qq.com>
2019-03-10 11:44:26 +0800

    v-1.4 login页面样式引入

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,14 +1,65 @@
 <template>
-    <div>
-        登录
-    </div>
+  <div class="login">
+    <el-form
+      :model="formData"
+      status-icon
+      ref="ruleForm2"
+      label-width="100px"
+      class="demo-ruleForm"
+      label-position="top"
+    >
+    <h2>用户登录</h2>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model.number="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="formData.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="button" type="primary" @click="submitForm('formData')">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script>
 export default {
-    
-}
+    data() {
+      return {
+        formData: {
+          username: '',
+          password: '',
+        },
+      };
+    },
+};
 </script>
 <style>
-
+body,html{
+    height: 100%;
+    width: 100%;
+}
+body{
+    background-color: #324152;
+    padding: 0;
+    margin: 0;
+}
+body>div {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login {
+    width: 580px;
+    height: 436px;
+    background-color: #fff;
+    padding: 40px;
+    box-sizing: border-box;
+    border-radius: 10px;
+}
+.login .button{
+    width: 100%;
+}
 </style>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
 
+// 导入elementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// 导入路由
 import router from './lib/router'
+
 new Vue({
+  router,
   render: h => h(App),
-  router
+  
 }).$mount('#app')
